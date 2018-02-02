e6e3b763a85da3f8c9b3205ad19c18a90beb275c
Sergei Poznanski <spoznanski@amberity.com>
2018-02-02 14:02:46 +0200

    Initial commit

---
+++ first-app/src/main/java/io/github/cepr0/firstapp/FirstApplication.java
@@ -0,0 +1,21 @@
+package io.github.cepr0.firstapp;
+
+import org.h2.tools.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+	
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server h2Server() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+	}
+}
